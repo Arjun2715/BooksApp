a5bc49c034cd50027f1aa17e66109c8c0f018180
Arjun2715 <arjunjot18@gmail.com>
2023-01-10 11:57:09 +0100

    info Activity

--- app/src/main/java/com/example/activity3v1/MainActivity.java
+++ app/src/main/java/com/example/activity3v1/MainActivity.java
@@ -10,9 +10,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
     Button cart;
     Button info;
     Button menu;
-
+    Button menuButton;
+    DrawerLayout drawer;
 
 
     @Override
@@ -39,14 +46,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tabLayout = findViewById(R.id.tabLayout);
         viewPager = findViewById(R.id.viewPager);
-        cart = findViewById(R.id.cart);
-        info = findViewById(R.id.info);
         menu = findViewById(R.id.menu);
 
         getTabs();
-        openCart();
-        openInfo();
         openMenu();
+
+        //joan
+        menuButton = findViewById(R.id.menu);
+        drawer = findViewById(R.id.drawer_layout);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else {
+                    drawer.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        //listeners
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_item_1:
+                        // Handle the Cart item click
+                        Intent orderDetailsIntent = new Intent(MainActivity.this, OrderDetails.class);
+                        startActivity(orderDetailsIntent);
+                        return true;
+                    case R.id.nav_item_2:
+                        // Handle the Info item click
+                        Intent orderDetailsIntent2 = new Intent(MainActivity.this, ActivityInfo.class);
+                        startActivity(orderDetailsIntent2);
+
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
     }
     public void openMenu(){
         menu.setOnClickListener(new View.OnClickListener() {
@@ -61,13 +104,19 @@ public class MainActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         startActivity(intent);
     }
+    private void openTopInfo() {
+        Intent intent = new Intent(this, ActivityInfo.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
+    }
 
 
     public void openInfo(){
         info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                diaplayToast("No Info Available");
+                diaplayToast("Info");
+                openTopInfo();
             }
         });
     }
@@ -122,17 +171,6 @@ public class MainActivity extends AppCompatActivity {
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.info:
-                displayToast(getString(R.string.action_order_message));
-                return true;
-            default:
-                // Do nothing
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
